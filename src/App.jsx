d2da4e88cb8d98082149b53cb719f036ff961833
Robert Garcia <rgs2@icomp.ufam.edu.br>
2022-02-11 17:14:04 -0400

    feat: create component of the repositorylist

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './styles/global.scss';
+import { RepositoryList } from './components/RepositoryList';
 
 export function App() {
-  return <h1>I'll be gone</h1>
+  return <RepositoryList/>
 }
---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,18 @@
+export function RepositoryList() {
+  return (
+    <section>
+      <h1>Lista de repositório</h1>
+
+      <ul>
+        <li>
+          <strong>Nome do repositório</strong>
+          <p>description</p>
+
+          <a href="">
+            Acessar o repositório
+          </a>
+        </li>
+      </ul>
+    </section>
+  );
+}
